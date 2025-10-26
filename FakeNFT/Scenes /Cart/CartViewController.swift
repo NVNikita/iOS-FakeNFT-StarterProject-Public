0bfe9c6bcd39d79bb1f331dfca66c5a2d146640f
Nikita <NVnagornyi@gmail.com>
2025-10-26 16:24:10 +0300

    add UITableView in cartVC

--- FakeNFT/Scenes /Cart/CartViewController.swift
+++ FakeNFT/Scenes /Cart/CartViewController.swift
@@ -9,8 +9,52 @@ import UIKit
 
 final class CartViewController: UIViewController {
     
+    private lazy var nftTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupTableView()
+        setupConstarints()
     }
     
+    private func setupNavigationBar() {
+        //TODO: - to do nav bar
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = UIColor.background
+        
+        view.addSubview(nftTableView)
+        
+        nftTableView.translatesAutoresizingMaskIntoConstraints = false
+        nftTableView.backgroundColor = .clear
+    }
+    
+    private func setupTableView() {
+        nftTableView.delegate = self
+        nftTableView.dataSource = self
+        nftTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    private func setupConstarints() {
+        NSLayoutConstraint.activate([
+            nftTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            nftTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            nftTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            nftTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+}
+
+extension CartViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
 }
