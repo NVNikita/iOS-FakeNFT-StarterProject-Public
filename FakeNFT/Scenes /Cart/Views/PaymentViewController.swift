bbd6c147958b2ae8d543285c574aa509d134d859
Nikita <NVnagornyi@gmail.com>
2025-10-31 17:42:24 +0300

    add PaymentViewController

---
+++ FakeNFT/Scenes /Cart/Views/PaymentViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PaymentViewController.swift
+//  FakeNFT
+//
+//  Created by Никита Нагорный on 31.10.2025.
+//
+
+import UIKit
+
+final class PaymentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
