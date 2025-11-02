d57e2b4d07ec6c8a7431c95ce47e39c712f56df0
Nikita <NVnagornyi@gmail.com>
2025-11-02 13:05:03 +0300

    add footerStackView and payButton

--- FakeNFT/Scenes /Cart/Views/PaymentViewController.swift
+++ FakeNFT/Scenes /Cart/Views/PaymentViewController.swift
@@ -9,16 +9,35 @@ import UIKit
 
 final class PaymentViewController: UIViewController {
     
+    private lazy var footerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 16
+        stackView.layer.masksToBounds = true
+        stackView.layer.cornerRadius = 12
+        stackView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        stackView.backgroundColor = UIColor.yaLightGrayLight
+        return stackView
+    }()
     
+    private lazy var payButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Оплатить", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 17, weight: .bold)
+        button.backgroundColor = .black
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 16
+        button.addTarget(self, action: #selector(payButtonTap), for: .touchUpInside)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupNavigationBar()
-    }
-    
-    private func setupUI() {
-        view.backgroundColor = .white
+        setupConstaints()
     }
     
     private func setupNavigationBar() {
@@ -39,11 +58,35 @@ final class PaymentViewController: UIViewController {
         navigationController?.navigationBar.isTranslucent = false
     }
     
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(footerStackView)
+        footerStackView.addSubview(payButton)
+        
+        footerStackView.translatesAutoresizingMaskIntoConstraints = false
+        payButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
     private func setupCollectionView() {
         
     }
     
     private func setupConstaints() {
+        NSLayoutConstraint.activate([
+            footerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            footerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            footerStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            footerStackView.heightAnchor.constraint(equalToConstant: 186),
+            
+            payButton.leadingAnchor.constraint(equalTo: footerStackView.leadingAnchor, constant: 20),
+            payButton.trailingAnchor.constraint(equalTo: footerStackView.trailingAnchor, constant: -12),
+            payButton.topAnchor.constraint(equalTo: footerStackView.topAnchor, constant: 76),
+            payButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        ])
+    }
+    
+    @objc private func payButtonTap() {
         
     }
 }
